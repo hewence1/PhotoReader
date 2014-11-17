60edf8ec1fb0cb91d83aef1274c707fee970e18a
hewence1 <603406320@qq.com>
2014-11-17 17:41:33 +0800

    add  commit
    add a commit

--- src/com/example/localphotoreader/adapter/MyAllPhotoAdapter.java
+++ src/com/example/localphotoreader/adapter/MyAllPhotoAdapter.java
@@ -16,7 +16,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.BaseAdapter;  //////////////hehheh
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
